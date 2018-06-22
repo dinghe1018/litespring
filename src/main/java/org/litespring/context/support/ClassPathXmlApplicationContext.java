b1ea708edaea28d988f0752ac47a7e1affe82532
dinghe <dinghe1018@163.com>
2018-06-22 20:56:46 +0800

    first commit

---
+++ src/main/java/org/litespring/context/support/ClassPathXmlApplicationContext.java
@@ -0,0 +1,16 @@
+package org.litespring.context.support;
+
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.Resource;
+
+public class ClassPathXmlApplicationContext extends AbstractApplicationContext {
+
+
+    public ClassPathXmlApplicationContext(String configFile) {
+        super(configFile);
+    }
+
+    protected Resource getResourceByPath(String path) {
+        return new ClassPathResource(path,this.getBeanClassLoader());
+    }
+}
---
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -0,0 +1,67 @@
+package org.litespring.test.v1;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanCreationException;
+import org.litespring.beans.factory.BeanDefinitionStoreException;
+import org.litespring.beans.factory.support.DefaultBeanFactory;
+import org.litespring.beans.factory.xml.XmlBeanDefinitionReader;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.service.v1.PetStoreService;
+
+
+public class BeanFactoryTest {
+    DefaultBeanFactory factory = null;
+    XmlBeanDefinitionReader reader = null;
+
+    @Before
+    public void setUp() {
+        factory = new DefaultBeanFactory();
+        reader = new XmlBeanDefinitionReader(factory);
+    }
+
+    @Test
+    public void testGetBean() {
+
+        reader.loadBeanDefinitions(new ClassPathResource("petstore-v1.xml"));
+
+        BeanDefinition bd = factory.getBeanDefinition("petStore");
+
+        Assert.assertTrue(bd.isSingleton());
+        Assert.assertFalse(bd.isPrototype());
+        Assert.assertEquals(BeanDefinition.SCOPE_DEFAULT,bd.getScope());
+
+        Assert.assertEquals("org.litespring.service.v1.PetStoreService", bd.getBeanClassName());
+        PetStoreService petStore = (PetStoreService) factory.getBean("petStore");
+        Assert.assertNotNull(petStore);
+
+        PetStoreService petStore1 = (PetStoreService)factory.getBean("petStore");
+        Assert.assertTrue(petStore.equals(petStore1));
+    }
+
+    @Test
+    public void testInvalidBean() {
+
+        reader.loadBeanDefinitions(new ClassPathResource("petstore-v1.xml"));
+        try {
+            factory.getBean("invalidBean");
+        } catch (BeanCreationException e) {
+            return;
+        }
+        Assert.fail("expect BeanCreationException");
+    }
+
+    @Test
+    public void testInvalidXML() {
+
+        try {
+
+            reader.loadBeanDefinitions(new ClassPathResource("xxx.xml"));
+        } catch (BeanDefinitionStoreException e) {
+            return;
+        }
+        Assert.fail("expect BeanDefinitionStoreException ");
+    }
+}
